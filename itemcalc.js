c3dd34473f54c0a00e4007b91f126dbcba52e816
WorkablePuffin <connorw.nc@gmail.com>
2024-12-02 14:55:11 -0500

    Update itemcalc.js

--- itemcalc.js
+++ itemcalc.js
@@ -18,36 +18,42 @@ function calculateHPHealed(item, numberofitems, currentHP){
             let flat = 0
             avg = ((dice*4.5)+flat) * numberofitems
             return avg
+            break
         }
         case 2: {
             let dice = 2
             let flat = 5
             avg = ((dice*4.5)+flat) * numberofitems
             return avg
+            break
         }
         case 3: {
             let dice = 3
             let flat = 10
             avg = ((dice*4.5)+flat) * numberofitems
             return avg
+            break
         }
         case 4: {
             let dice = 6
             let flat = 20
             avg = ((dice*4.5)+flat) * numberofitems
             return avg
+            break
         }
         case 5: {
             let dice = 8
             let flat = 30
             avg = ((dice*4.5)+flat) * numberofitems
             return avg
+            break
         }
         case 6: {
             let dice = 1
             let flat = 0
             avg = (dice*3.5)+flat * numberofitems
             return avg
+            break
         }
         case 7: {
             let dice = 3
@@ -60,24 +66,28 @@ function calculateHPHealed(item, numberofitems, currentHP){
             let flat = 18
             avg = (dice*3.5)+flat * numberofitems
             return avg
+            break
         }
         case 9: {
             let dice = 7
             let flat = 21
             avg = (dice*3.5)+flat * numberofitems
             return avg
+            break
         }
         case 10: {
             let dice = 8
             let flat = 21
             avg = (dice*3.5)+flat * numberofitems
             return avg
+            break
         }
         case 11: {
             let dice = 10
             let flat = 27
             avg = (dice*3.5)+flat * numberofitems
             return avg
+            break
         }
     }
 };
